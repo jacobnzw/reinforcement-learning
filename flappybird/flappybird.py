6d185f2d778ecf00e1334dfdadbe442051b30bd0
Jakub Prüher <jacobnzw@gmail.com>
2025-11-12 11:14:45 +0100

    Fix compute_returns, move tensor conversion inside, check gamma.

--- flappybird/flappybird.py
+++ flappybird/flappybird.py
@@ -117,50 +117,84 @@ class FlappyBirdStatePolicy(nn.Module):
         return action.item(), dist.log_prob(action)
 
 
-def compute_returns(rewards, gamma):
+def make_env(
+    env_id,
+    render_mode="rbg_array",
+    record_stats=False,
+    max_episode_steps: int | None = 10_000,
+    stack_size: int | None = None,
+    **kwargs,
+):
+    """Make the environment."""
+    env = gym.make(env_id, render_mode=render_mode, **kwargs)
+    if record_stats:
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+    if max_episode_steps:
+        env = gym.wrappers.TimeLimit(env, max_episode_steps)
+    if stack_size:
+        env = gym.wrappers.FrameStack(env, stack_size)
+    return env
+
+
+def compute_returns(rewards, gamma, normalize=True, device="cuda"):
     """Compute the returns from the rewards."""
+    if gamma <= 0.0 or gamma > 1.0:
+        raise ValueError(f"Invalid gamma: {gamma}. Should be in range (0, 1].")
+
     returns = deque(maxlen=len(rewards))
     discounted_return = 0
     for r in reversed(rewards):
         discounted_return = r + gamma * discounted_return
         # Using deque to prepend in O(1) to keep the returns in chronological order.
         returns.appendleft(discounted_return)
+
+    # Standardize the returns to make the training more stable
+    returns = torch.tensor(returns, device=device, dtype=torch.float32)
+    if normalize:
+        eps = np.finfo(np.float32).eps.item()  # Guard against division by zero (std=0)
+        returns = (returns - returns.mean()) / (returns.std() + eps)
+
     return returns
 
 
-def reinforce_episode(policy, env, max_t, gamma, entropy_coef: float | None = None):
+def reinforce_episode(policy, env, gamma, entropy_coef: float | None = None):
+    if env.spec.max_episode_steps is None:
+        raise ValueError(
+            "Env must have a finite max episode length. Check if env is wrapped in TimeLimit."
+        )
+
     log_probs = []
     rewards = []
     state, _ = env.reset()  # TODO: seed?
 
     # Collect trajectory: run the whole episode
-    for t in range(max_t):  # could be handled via gymnasium.wrappers.TimeLimit
+    episode_over = False
+    while not episode_over:  # expecting gymnasium.wrappers.TimeLimit applied on env
         action, log_prob = policy.act(state)
         state, reward, terminated, truncated, _ = env.step(action)
 
         log_probs.append(log_prob)
         rewards.append(reward)
 
-        if terminated or truncated:
-            break
+        episode_over = terminated or truncated
 
     # Calculate the return
-    returns = compute_returns(rewards, gamma)
-    # Standardize the returns to make the training more stable
-    returns = torch.tensor(returns)
-    eps = np.finfo(np.float32).eps.item()  # Guard against division by zero (std=0)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
+    returns = compute_returns(rewards, gamma, normalize=True, device=device)
 
     # Calculate the policy loss
-    loss = -torch.tensor(log_probs).dot(returns)
+    # torch.stack and @ preserve gradients (doesn't break computation graph as opposed to torch.tensor and .dot())
+    loss = -torch.stack(log_probs).T @ returns
 
     # Add the entropy term if specified
-    if entropy_coef:
-        loss += entropy_coef * sum(
-            [Categorical(logits=logits).entropy() for logits in log_probs]
-        )
+    entropy_term = (
+        entropy_coef
+        * sum([Categorical(logits=logits).entropy() for logits in log_probs])
+        if entropy_coef
+        else 0.0
+    )
+    loss += entropy_term
 
-    return loss, sum(rewards)
+    return loss, sum(rewards), entropy_term
 
 
 def reinforce_train_loop(
@@ -168,7 +202,6 @@ def reinforce_train_loop(
     env,
     learning_rate,
     n_training_episodes,
-    max_t,
     gamma,
     print_every=100,
     entropy_coef: float | None = None,
@@ -188,7 +221,6 @@ def reinforce_train_loop(
         optimizer (optim.Optimizer): Optimizer
         env (gymmasium.Env): Environment
         n_training_episodes (int): Number of training episodes
-        max_t (int): Maximum number of timesteps per episode
         gamma (float): Discount factor
         print_every (int): Print after this many episodes
     """
@@ -199,7 +231,9 @@ def reinforce_train_loop(
     # TODO: wrap env in RecordVideo and RecordEpisodeStatistics
 
     for i_episode in range(1, n_training_episodes + 1):
-        loss, summed_reward = reinforce_episode(policy, env, max_t, gamma, entropy_coef)
+        loss, summed_reward, entropy_term = reinforce_episode(
+            policy, env, gamma, entropy_coef
+        )
 
         # Update the policy parameters w/ optimizer based on gradients computed during backward pass
         optimizer.zero_grad()
@@ -209,30 +243,42 @@ def reinforce_train_loop(
 
         if i_episode % print_every == 0:
             print(
-                f"Episode {i_episode:6d} | Loss: {loss.item(): 2.4f} | Summed Reward: {np.mean(summed_reward): 2.4f}"
+                f"Episode {i_episode:6d} | Reward Sum: {summed_reward: 2.4f} | "
+                f"Loss: {loss.item(): 2.4f} | Entropy: {entropy_term: 2.4f}"
             )
 
 
 if __name__ == "__main__":
     env_id = "FlappyBird-v0"
     # TODO: FrameStack can still work even for state observations
-    env = gym.make(env_id, render_mode="rgb_array", use_lidar=False)
+    env = make_env(
+        env_id, render_mode="rgb_array", max_episode_steps=10_000, use_lidar=False
+    )
+
+    env = gym.wrappers.RecordVideo(
+        env, "videos/", episode_trigger=lambda e: e % 1000 == 0
+    )
+
+    # Set seeds for reproducibility
+    seed = 42
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    env.reset(seed=seed)
 
     hpars = {
-        "n_training_episodes": 10_000,
+        "n_training_episodes": 1,
         "n_evaluation_episodes": 10,
-        "max_t": 10_000,
+        "max_episode_steps": 10_000,
         "gamma": 0.99,
         "lr": 1e-4,
         "env_id": env_id,
         "entropy_coeff": None,
-        # TODO: Include seeds for reproducibility
+        "seed": seed,
     }
 
-    torch.manual_seed(50)
     policy_file = "model_from_hf.pt"
     exists = os.path.exists(policy_file)
-    reload = True
+    reload = False
     policy = (
         torch.load(policy_file, weights_only=False, map_location=device)
         if exists and reload
@@ -245,18 +291,17 @@ if __name__ == "__main__":
         env,
         hpars["lr"],
         hpars["n_training_episodes"],
-        hpars["max_t"],
         hpars["gamma"],
         entropy_coef=hpars["entropy_coeff"],
     )
-
+    env.close()
     torch.save(policy, "flappybird_policy.pt")
 
     # Hugging Face repo ID
-    model_id = f"Reinforce-{env_id}"
-    repo_id = f"jacobnzw/{model_id}"
-    eval_env = gym.make(env_id, render_mode="rgb_array", use_lidar=False)
+    # model_id = f"Reinforce-{env_id}"
+    # repo_id = f"jacobnzw/{model_id}"
+    # eval_env = gym.make(env_id, render_mode="rgb_array", use_lidar=False)
     # push_to_hub(repo_id, env_id, policy, hpars, eval_env)
     # TODO: better recording with gymnasium RecordVideo and RecordEpisodeStatistics
     # TODO: better reproducible eval per Grok
-    record_video(eval_env, policy, "flappybird_reinforce.mp4", fps=30)
+    # record_video(eval_env, policy, "flappybird_reinforce.mp4", fps=30)
