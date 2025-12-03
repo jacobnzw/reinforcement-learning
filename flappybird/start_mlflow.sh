423337fe3531c2e486626d2ec5033d9f775f273b
Jakub Prüher <jacobnzw@gmail.com>
2025-12-03 12:57:51 +0100

    Remove flappybird

--- flappybird/start_mlflow.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-BACKEND_STORE_URI="sqlite:///mlflow_tracking/mlruns.db"
-ARTIFACTS_DESTINATION="./mlflow_tracking/mlartifacts"
-
-uv run mlflow server \
---backend-store-uri $BACKEND_STORE_URI \
---artifacts-destination $ARTIFACTS_DESTINATION \
---default-artifact-root $ARTIFACTS_DESTINATION
